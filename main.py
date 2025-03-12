36c1cb5e6becf8c511bd0b362c1a6e8f641cb3f3
Alias2806 <155130924+Alias2806@users.noreply.github.com>
2025-03-12 17:06:50 +0800

    add machine problem

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""
 # EASY PROBLEM
 from math import factorial
 print ("EASY PROBLEM")
@@ -10,12 +11,13 @@ print("Output: " + str(factorial(num)))
 print("\nEASY PROBLEM alt solution")
 #accept user input
 num2 = int(input("Input: "))
+temp_num = num2
 #set default value for res as 1 since 0! is 1
 res = 1
 for i in range(num2):
     #multiply num2 and decrement and store into res
-    res *= num2
-    num2 -= 1
+    res *= temp_num
+    temp_num -= 1
 #print output
 print("Output: " + str(res))
 
@@ -33,6 +35,7 @@ for i in range(len(input_string)):
         count += 1
     map[input_string[i]] = count
 
+
 #create result string
 result = ""
 #loop through input string and check if the character has a value equal to 1, meaning it is unique
@@ -41,5 +44,51 @@ for i in range(len(input_string)):
         result += input_string[i] + " "
 #print out the result
 print(result)
+"""
+num_of_items_purchased = int(input("Enter the number of items purchased: "))
+customer_type = input("Enter customer type (Regular/VIP): ")
+total_price: float = 0
+item_totals = []
+item_prices = []
+item_quantities = []
+item_names = []
+
+def selectProduct():
+    item_name = input("Item name: ")
+    item_price = float(input("Item price: "))
+    item_quantity = float(input("Item quantity: "))
+    item_names.append(item_name)
+    item_prices.append(item_price)
+    item_quantities.append(item_quantity)
+    item_totals.append(item_quantity * item_price)
+    return item_price * item_quantity
 
 
+
+for i in range(1, num_of_items_purchased + 1):
+    print("Enter details for item " + str(i))
+    total_price += selectProduct()
+
+subtotal = total_price
+
+vip_discount: float = 0
+if customer_type.lower() == "vip":
+    vip_discount = total_price * 0.1
+    total_price = total_price - (total_price * 0.1)
+
+tax: float = 0
+if total_price > 500:
+    tax = total_price * 0.05
+    total_price = total_price + (total_price * 0.05)
+
+
+print("=======Grocery Store Receipt=======")
+for i in range(len(item_names)):
+    print("Item: " + str(item_names[i]) + "| Qty: " + str(item_quantities[i]) + "| Price: PHP" + str(item_prices[i]) + "| Total: PHP" + str(item_totals[i]))
+print("----------------------------------")
+print("Subtotal: PHP" + str(subtotal))
+print("VIP Discount (10%): -PHP" + str(vip_discount))
+print("Tax (5%): +PHP" + str(tax))
+print("----------------------------------")
+print("Final Total: PHP" + str(total_price))
+print("Thank you for shopping with us!")
